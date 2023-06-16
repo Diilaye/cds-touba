43b9ccf020c88a159884c74c01e6bd3689a7fa84
Diilaye <issa.k@verumko.com>
2023-06-16 17:57:37 +0000

    add new ffeatures

--- app.js
+++ app.js
@@ -9,6 +9,38 @@ const port = 5200;
 app.use(express.static(__dirname + '/web'));
 
 app.get('/', (req, res) => res.sendFile(path.resolve('web/index.html')));
+app.get('/forgetPassword', function(req, res) {
+  if (req.query.code==undefined) {
+   return res.redirect('/');
+  }
+    res.sendFile(__dirname + "/views/password.html")
+  });
+
+app.post('/forgetPassword' , (req,res) => {
+
+
+  let {password} = req.body ;
+
+  const url = 'https://api.cds-toubaouest.fr/v1/api/users/verif-code'
+    const data = {
+        password
+    };
+    const customHeaders = {
+        "Content-Type": "application/json",
+    }
+
+fetch(url, {
+    method: "POST",
+    headers: customHeaders,
+    body: JSON.stringify(data),
+})
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        res.redirect('/');
+    });
+
+});
 
 const server = http.createServer(app);
 
