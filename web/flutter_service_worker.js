e4c0fbef4fc3936c011d92732987c4231f2b0136
Diilaye <issa.k@verumko.com>
2023-06-26 18:20:13 +0000

    add news features

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "7eccba52b18e7cd68db87edb1ef477bb",
-"index.html": "28f6d17f4a191837050a28467133a360",
-"/": "28f6d17f4a191837050a28467133a360",
-"main.dart.js": "71c05f8bf1324ec83e72f760e219d602",
+"index.html": "2440873aac8a8a797de4584e9a6c0f50",
+"/": "2440873aac8a8a797de4584e9a6c0f50",
+"main.dart.js": "5ce73643be74bb68fcb543be137b20d2",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "16e16148ec33e87658ea2e0ff6d39e7b",
 "icons/Icon-192.png": "16e16148ec33e87658ea2e0ff6d39e7b",
